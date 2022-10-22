ea7a0c54e12e77044d1dea3f90d868e56de52a4e
Patrick Bray <patrickbray17@gmail.com>
2022-10-22 14:25:23 -0400

    working constraints

--- rendez/optimizer.py
+++ rendez/optimizer.py
@@ -1,4 +1,67 @@
 import numpy as np
+import pandas as pd
+from ortools.sat.python import cp_model
 
-def optimize(nodes: np.array, edges: np.array):
-    pass
+model = cp_model.CpModel()
+
+
+def add_var(x):
+    model.new
+
+
+def optimize(nodes: pd.DataFrame, edges: pd.DataFrame):
+    """
+    Args:
+        nodes: Dataframe of nodes and attributes
+        edges: Dataframe of edges, 'source' and 'destination'
+
+    """
+    ending_nodes = [5, 6]  # TODO: do not hardcode
+    # Create Edge Variables
+    edge_vars = {}
+    for source, destination in zip(edges["source"], edges["destination"]):
+        edge_vars[source, destination] = model.NewBoolVar(
+            str(source) + "_" + str(destination)
+        )
+
+    # C1: Continuity Constraint
+    for i in range(1, 5):
+        model.Add(
+            cp_model.LinearExpr.Sum(
+                [edge for key, edge in edge_vars.items() if key[0] == i]
+            )
+            == cp_model.LinearExpr.Sum(
+                [edge for key, edge in edge_vars.items() if key[1] == i]
+            )
+        )
+    # C2: Starting Constraint
+    model.Add(
+        cp_model.LinearExpr.Sum(
+            [edge for key, edge in edge_vars.items() if key[0] == 0]
+        )
+        == 1
+    )
+    # C3: Ending Constraint
+    model.Add(
+        cp_model.LinearExpr.Sum(
+            [edge for key, edge in edge_vars.items() if key[1] in ending_nodes]
+        )
+        == 1
+    )
+
+    # Solve
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+    if status in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
+        print("Model sucessful")
+        return get_selected_edges(solver, edge_vars)
+    else:
+        print(f"Model failed with error code {status}")
+        return None
+
+
+def get_selected_edges(solver: cp_model.CpSolver, edge_vars: dict):
+    """
+    Extracts a solution from the solver and its variables
+    """
+    return [key for key, val in edge_vars.items() if solver.Value(val)]
--- scripts/test_optimizer.py
+++ scripts/test_optimizer.py
@@ -1,10 +1,28 @@
 import pandas as pd
 import rendez.optimizer
 
+
 def simple_test():
     edges = pd.read_csv("test_data/test_edges.csv")
     nodes = pd.read_csv("test_data/test_nodes.csv")
-    rendez.optimizer.optimize(nodes, edges)
+    soln = rendez.optimizer.optimize(nodes, edges)
+    assert_solution_valid(edges, soln)
+
+
+def assert_solution_valid(edges, soln):
+    print("Proposed Solution:\n", soln)
+    sources = set([s[0] for s in soln])
+    dests = set([s[1] for s in soln])
+    # Solution Length
+    assert len(soln) == 3, "Solution length incorrect"
+    # C1: Continuity Constraint
+    assert sources - dests == {0}, "Continuity constraint violated"
+    assert len(dests - sources) == 1, "Continuity constraint violated"
+    # C2: Starting Constraint
+    assert 0 in sources, "Starting constraint violated"
+    # C3: Ending Constraint
+    # assert sum([s[1]  for s in soln]) == 1, "Starting constraint violated"
+
 
 if __name__ == "__main__":
     simple_test()
